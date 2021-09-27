6fe15b563396fc92561b929d6fdf9e0bfbdd8662
Munir Arabi Marques <munirarabi04@gmail.com>
2021-09-27 12:32:48 -0300

    editing title

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ function App() {
   }
 
   const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+    let days = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"]
 
     let day = days[d.getDay()]
     let date = d.getDate()
